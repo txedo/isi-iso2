6ab8e4e83052a21a3175eb6bc5102396befe07c8
txedosuke <txedosuke@a24d8f6a-ae78-11de-b826-45995d83e13f>
2009-11-09 19:46:00 +0000

    Primera version de la interfaz del servidor. Falta por mostrar los mensajes de error de las excepciones de RMI

--- trunk/practica1/app/servidorFrontend/src/presentacion/JFServidorFrontend.java
+++ trunk/practica1/app/servidorFrontend/src/presentacion/JFServidorFrontend.java
@@ -1,15 +1,17 @@
 package presentacion;
 import com.cloudgarden.layout.AnchorConstraint;
 import com.cloudgarden.layout.AnchorLayout;
+import comunicaciones.ServidorFrontend;
+
 import java.awt.BorderLayout;
 
-import java.awt.Dimension;
-import java.awt.GridBagLayout;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,8 +19,6 @@ import javax.swing.JTextArea;
 
 import javax.swing.WindowConstants;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.SoftBevelBorder;
 import javax.swing.SwingUtilities;
 
 
@@ -35,27 +35,18 @@ import javax.swing.SwingUtilities;
 * LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
 */
 public class JFServidorFrontend extends javax.swing.JFrame {
+	private ServidorFrontend servidorFE;
 	private JPanel jPanel1;
+	private JLabel labelBarraEstado;
 	private JButton botonDesconectar;
 	private JButton botonConectar;
 	private JTextArea jTextArea1;
 
-	/**
-	* Auto-generated main method to display this JFrame
-	*/
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFServidorFrontend inst = new JFServidorFrontend();
-				inst.setLocationRelativeTo(null);
-				inst.setVisible(true);
-			}
-		});
-	}
 	
-	public JFServidorFrontend() {
+	public JFServidorFrontend(ServidorFrontend serv) {
 		super();
 		initGUI();
+		this.servidorFE = serv;
 	}
 	
 	private void initGUI() {
@@ -63,15 +54,37 @@ public class JFServidorFrontend extends javax.swing.JFrame {
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			this.setTitle("Servidor Front-End");
 			this.setAlwaysOnTop(true);
+			this.setPreferredSize(new java.awt.Dimension(374, 266));
+			this.setMinimumSize(new java.awt.Dimension(374, 266));
+
+			this.addWindowListener(new java.awt.event.WindowAdapter() { 
+				public void windowClosing(java.awt.event.WindowEvent e) {    
+					System.exit(0);
+				}
+			});
 
 			jPanel1 = new JPanel();
 			AnchorLayout jPanel1Layout = new AnchorLayout();
 				getContentPane().add(jPanel1, BorderLayout.CENTER);
 				jPanel1.setLayout(jPanel1Layout);
+<<<<<<< .mine
+				jPanel1.setPreferredSize(new java.awt.Dimension(374, 266));
+=======
 				jPanel1.setPreferredSize(new java.awt.Dimension(374, 252));
+>>>>>>> .r55
+				{
+					labelBarraEstado = new JLabel();
+					jPanel1.add(labelBarraEstado, new AnchorConstraint(937, 10, 3, 10, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS));
+					labelBarraEstado.setText("Servidor desconectado...");
+					labelBarraEstado.setPreferredSize(new java.awt.Dimension(354, 14));
+				}
 				{
 					botonDesconectar = new JButton();
+<<<<<<< .mine
+					jPanel1.add(botonDesconectar, new AnchorConstraint(10, 643, 124, 140, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+=======
 					jPanel1.add(botonDesconectar, new AnchorConstraint(12, 680, 173, 134, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+>>>>>>> .r55
 					botonDesconectar.setText("Desconectar");
 					botonDesconectar.setPreferredSize(new java.awt.Dimension(116, 30));
 					botonDesconectar.setEnabled(false);
@@ -83,10 +96,17 @@ public class JFServidorFrontend extends javax.swing.JFrame {
 				}
 				{
 					botonConectar = new JButton();
+<<<<<<< .mine
+					jPanel1.add(botonConectar, new AnchorConstraint(10, 322, 165, 10, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+=======
 					jPanel1.add(botonConectar, new AnchorConstraint(12, 330, 173, 13, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+>>>>>>> .r55
 					botonConectar.setText("Conectar");
 					botonConectar.setPreferredSize(new java.awt.Dimension(110, 30));
+<<<<<<< .mine
+=======
 					botonConectar.setAutoscrolls(true);
+>>>>>>> .r55
 					botonConectar.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent evt) {
 							botonConectarActionPerformed(evt);
@@ -95,7 +115,12 @@ public class JFServidorFrontend extends javax.swing.JFrame {
 				}
 				{
 					jTextArea1 = new JTextArea();
+<<<<<<< .mine
+					jPanel1.add(jTextArea1, new AnchorConstraint(56, 10, 28, 10, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS));
+					jTextArea1.setPreferredSize(new java.awt.Dimension(354, 182));
+=======
 					jPanel1.add(jTextArea1, new AnchorConstraint(54, 12, 11, 13, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS));
+>>>>>>> .r55
 					jTextArea1.setEditable(false);
 					jTextArea1.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
 					jTextArea1.setPreferredSize(new java.awt.Dimension(349, 187));
@@ -109,13 +134,36 @@ public class JFServidorFrontend extends javax.swing.JFrame {
 	}
 	
 	private void botonConectarActionPerformed(ActionEvent evt) {
-		System.out.println("botonConectar.actionPerformed, event="+evt);
-		//TODO add your code for botonConectar.actionPerformed
+		try {
+			this.servidorFE.conectar();
+			this.botonConectar.setEnabled(false);
+			this.botonDesconectar.setEnabled(true);
+			this.labelBarraEstado.setText("Servidor conectado...");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private void botonDesconectarActionPerformed(ActionEvent evt) {
-		System.out.println("botonDesconectar.actionPerformed, event="+evt);
-		//TODO add your code for botonDesconectar.actionPerformed
+		try {
+			this.servidorFE.desconectar();
+			this.botonDesconectar.setEnabled(false);
+			this.botonConectar.setEnabled(true);
+			this.labelBarraEstado.setText("Servidor desconectado...");
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
