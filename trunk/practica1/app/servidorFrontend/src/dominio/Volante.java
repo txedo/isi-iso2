4bb02ad81275e4f04c1e4c6f5776d08a9740bbc9
juanro.1987 <juanro.1987@a24d8f6a-ae78-11de-b826-45995d83e13f>
2009-12-16 11:33:38 +0000

    - Modificado install para añadir la tabla de volantes y nuevos usuarios a la BD - Creada la clase Volante - Modificado el GestorCitas para almacenar la cita con volantes
    ** FALTA: modificar la gestion de beneficiarios para asignar el medico entre cabecera o pediatra, no aleatorio **

--- trunk/practica1/app/servidorFrontend/src/dominio/Volante.java
+++ trunk/practica1/app/servidorFrontend/src/dominio/Volante.java
@@ -2,4 +2,52 @@ package dominio;
 
 public class Volante {
 
+	long id;
+	Medico emisor;
+	Medico receptor;
+	Beneficiario beneficiario;
+	
+	public Volante () {}
+	
+	public Volante(long id, Medico emisor, Medico receptor, Beneficiario beneficiario) {
+		this.id = id;
+		this.emisor = emisor;
+		this.receptor = receptor;
+		this.beneficiario = beneficiario;
+	}
+
+	public Medico getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(Medico emisor) {
+		this.emisor = emisor;
+	}
+
+	public Medico getReceptor() {
+		return receptor;
+	}
+
+	public void setReceptor(Medico receptor) {
+		this.receptor = receptor;
+	}
+
+	public Beneficiario getBeneficiario() {
+		return beneficiario;
+	}
+
+	public void setBeneficiario(Beneficiario beneficiario) {
+		this.beneficiario = beneficiario;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	
+
 }
