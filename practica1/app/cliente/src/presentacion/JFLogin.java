8f9efc41ee42ef44bbb5d0fd9a2dffeeb66c787f
Txedo <txedo@users.noreply.github.com>
2009-12-20 18:33:08 +0000

    Unidad de citación: * Mejorada la apariencia del JFLogin * Añadida la posibilidad de especificar la direción IP del servidor-frontend

--- practica1/app/cliente/src/presentacion/JFLogin.java
+++ practica1/app/cliente/src/presentacion/JFLogin.java
@@ -6,6 +6,7 @@ import com.sun.org.apache.xerces.internal.impl.dtd.models.DFAContentModel;
 
 import dominio.ControladorLogin;
 import excepciones.UsuarioIncorrectoException;
+import java.awt.BorderLayout;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +15,7 @@ import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
@@ -43,9 +45,11 @@ public class JFLogin extends javax.swing.JFrame {
 
 	
 	private ControladorLogin controlador;
-	private JLabel jLabel1;
 	private JLabel jLabel2;
 	private JTextField txtUsuario;
+	private JLabel jLabel4;
+	private JTextField txtIPServidor;
+	private JPanel jPanel1;
 	private JButton btnAvanzado;
 	private JButton btnConectar;
 	private JPasswordField txtPassword;
@@ -58,68 +62,91 @@ public class JFLogin extends javax.swing.JFrame {
 	
 	private void initGUI() {
 		try {
-			AnchorLayout thisLayout = new AnchorLayout();
+			BorderLayout thisLayout = new BorderLayout();
 			getContentPane().setLayout(thisLayout);
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-			this.setTitle("Identificación");
-			this.setPreferredSize(new java.awt.Dimension(383, 178));
-			this.setMinimumSize(new java.awt.Dimension(383, 178));
+			this.setTitle("Inicio de sesión");
+			this.setPreferredSize(new java.awt.Dimension(296, 140));
+			this.setMinimumSize(new java.awt.Dimension(296, 140));
 			this.setMaximumSize(new java.awt.Dimension(2147483647, 178));
 			this.setResizable(false);
 			{
-				btnAvanzado = new JButton();
-				getContentPane().add(btnAvanzado, new AnchorConstraint(748, 266, 16, 27, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE));
-				btnAvanzado.setText("Avanzado >>");
-				btnAvanzado.setPreferredSize(new java.awt.Dimension(101, 23));
-				btnAvanzado.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent evt) {
-						btnAvanzadoActionPerformed(evt);
-					}
-				});
+				jPanel1 = new JPanel();
+				AnchorLayout jPanel1Layout = new AnchorLayout();
+				getContentPane().add(jPanel1, BorderLayout.CENTER);
+				jPanel1.setLayout(jPanel1Layout);
+				jPanel1.setPreferredSize(new java.awt.Dimension(296, 140));
+				jPanel1.setSize(296, 140);
+				jPanel1.setMinimumSize(new java.awt.Dimension(296, 140));
+				{
+					btnAvanzado = new JButton();
+					btnAvanzado.setLayout(null);
+					jPanel1.add(btnAvanzado, new AnchorConstraint(458, 291, 18, 10, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS));
+					btnAvanzado.setText("Avanzado >>");
+					btnAvanzado.setPreferredSize(new java.awt.Dimension(101, 23));
+					btnAvanzado.setRequestFocusEnabled(false);
+					btnAvanzado.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent evt) {
+							btnAvanzadoActionPerformed(evt);
+						}
+					});
+				}
+				{
+					btnConectar = new JButton();
+					jPanel1.add(btnConectar, new AnchorConstraint(450, 20, 18, 698, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE));
+					btnConectar.setDefaultCapable(true);
+					getRootPane().setDefaultButton(btnConectar);
+					btnConectar.setText("Iniciar sesión");
+					btnConectar.setSelected(true);
+					btnConectar.setPreferredSize(new java.awt.Dimension(97, 23));
+					btnConectar.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent evt) {
+							btnConectarActionPerformed(evt);
+						}
+					});
+				}
+				{
+					txtIPServidor = new JTextField();
+					jPanel1.add(txtIPServidor, new AnchorConstraint(65, 20, 678, 131, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+					txtIPServidor.setText("127.0.0.1");
+					txtIPServidor.setPreferredSize(new java.awt.Dimension(139, 20));
+					txtIPServidor.setLayout(null);
+				}
+				{
+					jLabel4 = new JLabel();
+					jPanel1.add(jLabel4, new AnchorConstraint(68, 443, 654, 10, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+					jLabel4.setText("Dirección IP del servidor");
+					jLabel4.setPreferredSize(new java.awt.Dimension(118, 14));
+				}
+				{
+					txtPassword = new JPasswordField();
+					jPanel1.add(txtPassword, new AnchorConstraint(40, 20, 440, 131, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+					txtPassword.setPreferredSize(new java.awt.Dimension(139, 20));
+					txtPassword.setLayout(null);
+				}
+				{
+					jLabel3 = new JLabel();
+					jPanel1.add(jLabel3, new AnchorConstraint(43, 446, 418, 10, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+					jLabel3.setText("Contraseña");
+					jLabel3.setPreferredSize(new java.awt.Dimension(119, 14));
+				}
+				{
+					txtUsuario = new JTextField();
+					jPanel1.add(txtUsuario, new AnchorConstraint(15, 20, 262, 131, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+					txtUsuario.setPreferredSize(new java.awt.Dimension(139, 20));
+					txtUsuario.setLayout(null);
+				}
+				{
+					jLabel2 = new JLabel();
+					jPanel1.add(jLabel2, new AnchorConstraint(18, 450, 240, 10, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
+					jLabel2.setText("Usuario");
+					jLabel2.setPreferredSize(new java.awt.Dimension(119, 14));
+				}
 			}
-			{
-				btnConectar = new JButton();
-				getContentPane().add(btnConectar, new AnchorConstraint(735, 202, 18, 267, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS));
-				btnConectar.setDefaultCapable(true);
-				getRootPane().setDefaultButton(btnConectar);
-				btnConectar.setText("Iniciar sesión");
-				btnConectar.setPreferredSize(new java.awt.Dimension(101, 23));
-				btnConectar.setSelected(true);
-				btnConectar.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent evt) {
-						btnConectarActionPerformed(evt);
-					}
-				});
-			}
-			{
-				txtPassword = new JPasswordField();
-				getContentPane().add(txtPassword, new AnchorConstraint(73, 10, 10, 99, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
-				txtPassword.setPreferredSize(new java.awt.Dimension(266, 21));
-			}
-			{
-				jLabel3 = new JLabel();
-				getContentPane().add(jLabel3, new AnchorConstraint(77, 10, 10, 12, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
-				jLabel3.setText("Contraseña: ");
-				jLabel3.setPreferredSize(new java.awt.Dimension(79, 14));
-			}
-			{
-				txtUsuario = new JTextField();
-				getContentPane().add(txtUsuario, new AnchorConstraint(39, 10, 10, 99, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
-				txtUsuario.setPreferredSize(new java.awt.Dimension(266, 21));
-			}
-			{
-				jLabel2 = new JLabel();
-				getContentPane().add(jLabel2, new AnchorConstraint(43, 10, 10, 12, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
-				jLabel2.setText("Usuario: ");
-				jLabel2.setPreferredSize(new java.awt.Dimension(79, 14));
-			}
-			{
-				jLabel1 = new JLabel();
-				getContentPane().add(jLabel1, new AnchorConstraint(13, 307, 245, 12, AnchorConstraint.ANCHOR_ABS, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_NONE, AnchorConstraint.ANCHOR_ABS));
-				jLabel1.setText("Inicio de sesión");
-				jLabel1.setPreferredSize(new java.awt.Dimension(103, 14));
-			}
+			txtIPServidor.setVisible(false);
+			jLabel4.setVisible(false);
 			pack();
+			this.setSize(296, 140);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -131,7 +158,7 @@ public class JFLogin extends javax.swing.JFrame {
 	
 	private void btnConectarActionPerformed(ActionEvent evt) {
 		try {
-			controlador.iniciarSesion("127.0.0.1", txtUsuario.getText(), new String(txtPassword.getPassword()));
+			controlador.iniciarSesion(txtIPServidor.getText(), txtUsuario.getText(), new String(txtPassword.getPassword()));
 		} catch (SQLException e) {
 			Utilidades.mostrarDialogoError(this, "Error en el sistema", e.getMessage());
 		} catch (UsuarioIncorrectoException e) {
@@ -144,12 +171,20 @@ public class JFLogin extends javax.swing.JFrame {
 	private void btnAvanzadoActionPerformed(ActionEvent evt) {
 		if (btnAvanzado.getText().equals("Avanzado >>")) {
 			btnAvanzado.setText("<< Avanzado");
-			this.setSize(new java.awt.Dimension(383, 250));
+			//Grande [296, 167]
+			this.setSize(new java.awt.Dimension(296, 167));
+			jPanel1.setSize(new java.awt.Dimension(296, 167));
+			txtIPServidor.setVisible(true);
+			jLabel4.setVisible(true);
 		}
 		else {
 			if (btnAvanzado.getText().equals("<< Avanzado")) {
 				btnAvanzado.setText("Avanzado >>");
-				this.setSize(new java.awt.Dimension(383, 178));
+				//Pequeño [296, 140]
+				this.setSize(new java.awt.Dimension(296, 140));
+				jPanel1.setSize(new java.awt.Dimension(296, 140));
+				txtIPServidor.setVisible(false);
+				jLabel4.setVisible(false);
 			}
 		}
 	}
