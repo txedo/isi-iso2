4f1d6be8ea72751a35785a930fd88fc3a7c55b65
juanguclm <juanguclm@a24d8f6a-ae78-11de-b826-45995d83e13f>
2010-01-18 12:30:12 +0000

    * Añadidas nuevas operaciones auxiliares para crear, modificar y eliminar usuarios (que no sean médicos).

--- practica1/app/servidorFrontend/src/dominio/control/GestorMensajes.java
+++ practica1/app/servidorFrontend/src/dominio/control/GestorMensajes.java
@@ -1,14 +1,39 @@
 package dominio.control;
 
+import java.sql.SQLException;
 import dominio.conocimiento.OperacionesAuxiliares;
+import dominio.conocimiento.Usuario;
+import excepciones.CentroSaludIncorrectoException;
+import excepciones.OperacionIncorrectaException;
 import excepciones.SesionInvalidaException;
+import excepciones.UsuarioInexistenteException;
+import excepciones.UsuarioYaExistenteException;
 
+/**
+ * Clase encargada de interpretar los mensajes auxiliares recibidos
+ * y reenviarlos a los gestores que correspondan.
+ */
 public class GestorMensajes implements OperacionesAuxiliares {
-	public static Object mensajeAuxiliar(long idSesion, long codigoMensaje, Object informacion) throws SesionInvalidaException {
-		Object res = null;
-		if (codigoMensaje == OPERACIONES_DISPONIBLES) { //operaciones disponibles
-			res = GestorSesiones.operacionesDisponibles(idSesion);
+	
+	public static Object mensajeAuxiliar(long idSesion, long codigoMensaje, Object informacion) throws SesionInvalidaException, SQLException, UsuarioYaExistenteException, OperacionIncorrectaException, CentroSaludIncorrectoException, UsuarioInexistenteException {
+		Object res;
+		
+		res = null;
+		switch((int)codigoMensaje) {
+			case OPERACIONES_DISPONIBLES:
+				res = GestorSesiones.operacionesDisponibles(idSesion);
+				break;
+			case CREAR_USUARIO:
+				GestorUsuarios.crearUsuario(idSesion, (Usuario)informacion);
+				break;
+			case MODIFICAR_USUARIO:
+				GestorUsuarios.modificarUsuario(idSesion, (Usuario)informacion);
+				break;
+			case ELIMINAR_USUARIO:
+				GestorUsuarios.eliminarUsuario(idSesion, (Usuario)informacion);
+				break;
 		}
 		return res;
 	}
+	
 }
