974d47fc53e9908ecea8fd4c200ddf0a21b6940d
juanro.1987 <juanro.1987@a24d8f6a-ae78-11de-b826-45995d83e13f>
2010-01-23 22:16:31 +0000

    - Una cosa que no puse en el cambio anterior:    * He cambiado la clase Usuario, ahora ya no recibe un Centro de salud en su constructor sino que, cuando se crea un usuario, se le asigna un centro de salud aleatorio, al igual que se hace con los beneficiarios y los medicos.

--- practica1/app/cliente/src/presentacion/JPCitaTramitar.java
+++ practica1/app/cliente/src/presentacion/JPCitaTramitar.java
@@ -4,14 +4,25 @@ import javax.swing.JLabel;
 import com.cloudgarden.layout.AnchorConstraint;
 import com.cloudgarden.layout.AnchorLayout;
 
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
 /**
 * Panel que permite tramitar citas para beneficiarios del sistema.
 */
 public class JPCitaTramitar extends JPBase {
 
 	private static final long serialVersionUID = 8297107492599580450L;
-	
-	private JLabel lblPrueba;
 
 	public JPCitaTramitar() {
 		super();
@@ -24,12 +35,6 @@ public class JPCitaTramitar extends JPBase {
 			this.setLayout(thisLayout);
 			this.setSize(430, 390);
 			this.setPreferredSize(new java.awt.Dimension(430, 390));
-			{
-				lblPrueba = new JLabel();
-				this.add(lblPrueba, new AnchorConstraint(32, 280, 73, 29, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
-				lblPrueba.setText("Tramitar cita");
-				lblPrueba.setPreferredSize(new java.awt.Dimension(108, 16));
-			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
