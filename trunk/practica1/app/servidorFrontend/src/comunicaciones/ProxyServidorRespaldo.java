248c9a9ca639ce3e00733f7b86eaf3d48ad0400c
juanguclm <juanguclm@a24d8f6a-ae78-11de-b826-45995d83e13f>
2010-03-15 17:18:01 +0000

    * Como no había tiempo de modificar el servidor front-end para que se diera cuenta de cuándo el servidor de respaldo se desconecta, lo que he hecho es modificar el ProxyServidorRespaldo (sólo esta clase) para que si el servidor de respaldo se desconecta y vuelve a conectar, no sea necesario desconectar y volver a desconectar el servidor front-end para que se mantenga la conexión. * Modificado el GestorConexionesBD para mostrar qué base de datos falla si se produce un error en una transacción. * Corregido un error por el cual los mensajes del servidor de respaldo podrían aparecer repetidos. * Añadido un comentario sobre las dificultades de hacer la interfaz gráfica a la documentación. * Añadidos los JAR de las 3 aplicaciones a sus respectivos directorios. ¡Comprobad que funcionan (aunque sea en local) antes de enviarlos!
    Este podría ser un esquema de los archivos que habría que subir a Moodle. No creo que se me olvide nada, ¡pero comprobadlo :)!
    - documentacion
       · documentacion.pdf
    - diagramas
       · (archivos VPP)
       · (si quereis, otros archivos de diagramas)
    - codigo
       - cliente
          - src
          - lib
          · Cliente.jar
       - frontend
          - src
          - lib
          · install.txt
          · install-pruebas.txt
          · ServidorFrontend.jar
          · ServidorFrontendStubs.jar
       - respaldo
          - src
          - lib
          · install.txt
          · ServidorRespaldo.jar
          · ServidorRespaldoStubs.jar
    - ejecutables
       · Cliente.jar
       · ServidorFrontend.jar
       · ServidorRespaldo.jar
    - basedatos
       · install-frontend.txt
       · install-respaldo.txt
    
    TODO: Comentar en la documentación que para hacer las pruebas del cliente se ha utilizado la base de datos "install-pruebas.txt" (que es la que tiene los usuarios "admin" y "admina").

--- trunk/practica1/app/servidorFrontend/src/comunicaciones/ProxyServidorRespaldo.java
+++ trunk/practica1/app/servidorFrontend/src/comunicaciones/ProxyServidorRespaldo.java
@@ -15,51 +15,117 @@ public class ProxyServidorRespaldo implements IServidorRespaldo {
 
 	private IServidorRespaldo servidor;
 	
+	private String ip;
+	private int puerto;
+	
 	public void conectar(String ip, int puerto) throws MalformedURLException, RemoteException, NotBoundException {
 		String url;
 		
+		this.ip = ip;
+		this.puerto = puerto;
 		url = "rmi://" + ip + ":" + String.valueOf(puerto) + "/" + NOMBRE_SERVIDOR;
         servidor = (IServidorRespaldo)Naming.lookup(url);
+	}
+	
+	private void reconectar() throws RemoteException {
+		String url;
+		
+		url = "rmi://" + ip + ":" + String.valueOf(puerto) + "/" + NOMBRE_SERVIDOR;
+        try {
+			servidor = (IServidorRespaldo)Naming.lookup(url);
+		} catch(NotBoundException e) {
+			throw new RemoteException(e.getMessage());
+		} catch(MalformedURLException e) {
+			throw new RemoteException(e.getMessage());
+		}
 	}
 
 	// Métodos de acceso a la base de datos
 	
 	public void abrir() throws RemoteException, SQLException {
-		servidor.abrir();
+		try {
+			servidor.abrir();
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.abrir();
+		}
 	}
 
 	public void cerrar() throws RemoteException, SQLException {
-		servidor.cerrar();
+		try {
+			servidor.cerrar();
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.cerrar();
+		}
 	}
 
 	public ResultSet consultar(ComandoSQL comando) throws RemoteException, SQLException {
-		return servidor.consultar(comando);
+		ResultSet datos;
+		
+		try {
+			datos = servidor.consultar(comando);
+		} catch(RemoteException e) {
+			reconectar();
+			datos = servidor.consultar(comando);
+		}
+		return datos;
 	}
 
 	public void ejecutar(ComandoSQL comando) throws RemoteException, SQLException {
-		servidor.ejecutar(comando);
+		try {
+			servidor.ejecutar(comando);
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.ejecutar(comando);
+		}
 	}
 
 	public void commit() throws RemoteException, SQLException {
-		servidor.commit();
+		try {
+			servidor.commit();
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.commit();
+		}
 	}
 
 	public void rollback() throws RemoteException, SQLException {
-		servidor.rollback();
+		try {
+			servidor.rollback();
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.rollback();
+		}
 	}
 
 	// Métodos de actualización del estado
 	
 	public void ponerMensaje(String tipoMensaje, String mensaje) throws RemoteException, SQLException {
-		servidor.ponerMensaje(tipoMensaje, mensaje);
+		try {
+			servidor.ponerMensaje(tipoMensaje, mensaje);
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.ponerMensaje(tipoMensaje, mensaje);
+		}
 	}
 
 	public void ponerMensaje(String usuario, String tipoMensaje, String mensaje) throws RemoteException, SQLException {
-		servidor.ponerMensaje(usuario, tipoMensaje, mensaje);
+		try {
+			servidor.ponerMensaje(usuario, tipoMensaje, mensaje);
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.ponerMensaje(usuario, tipoMensaje, mensaje);
+		}
 	}
 
 	public void actualizarClientesEscuchando(int numeroClientes) throws RemoteException, SQLException {
-		servidor.actualizarClientesEscuchando(numeroClientes);
+		try {
+			servidor.actualizarClientesEscuchando(numeroClientes);
+		} catch(RemoteException e) {
+			reconectar();
+			servidor.actualizarClientesEscuchando(numeroClientes);
+		}
 	}
 
 }
