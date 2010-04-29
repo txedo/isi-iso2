87ea97db43bf9f0b7624af3a59f82471fe13146d
juanro.1987 <juanro.1987@a24d8f6a-ae78-11de-b826-45995d83e13f>
2010-04-29 22:53:53 +0000

    - Borrado el index.jsp, porque dice el enunciado que se use index.html - Creadas algunas páginas Web según los prototipos del enunciado (falta por aplicar el estilo y dejarlo con buen diseño) - Mejorada la accion "loginBeneficiario" para conectarse al proxy y recuperar el beneficiario a partir del NSS introducido - Guardado el objeto Beneficiario en un HttpSession apra que se pueda recuperar sus datos en las paginas internas
    ** Revisar los TODO de la accion "loginBeneficiario"

--- trunk/practica2/app/AplicacionWeb/WebContent/anularCita.jsp
+++ trunk/practica2/app/AplicacionWeb/WebContent/anularCita.jsp
@@ -1,12 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ page import="dominio.conocimiento.Beneficiario" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Anular Cita</title>
 </head>
 <body>
-
+<%= ((Beneficiario) request.getSession(false).getAttribute("Beneficiario")).getNombre() %>, elija la cita que desee anular: <br>
+<span id="areaCitas">
+</span>
 </body>
 </html>
--- trunk/practica2/app/AplicacionWeb/src/acciones/loginBeneficiario.java
+++ trunk/practica2/app/AplicacionWeb/src/acciones/loginBeneficiario.java
@@ -1,6 +1,14 @@
 package acciones;
 
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+
 import com.opensymphony.xwork2.ActionSupport;
+import comunicaciones.ProxyServidorFrontend;
+
+import dominio.conocimiento.Beneficiario;
+import dominio.conocimiento.ISesion;
+import excepciones.UsuarioIncorrectoException;
 
 public class loginBeneficiario extends ActionSupport{
 	
@@ -10,8 +18,33 @@ public class loginBeneficiario extends ActionSupport{
 	private static final long serialVersionUID = 1830263215915906207L;
 	
 	private String Nss = "";
+	private Beneficiario beneficiario;
 		
 	public String execute() {
+		ProxyServidorFrontend p = new ProxyServidorFrontend();
+		ISesion s;
+		try {
+			// TODO: hacer que el porxy sea singleton y se conecte solo la primera vez que se necesite
+			// Esa vez será en el loginBeneficiario o en el loginMedico
+			p.conectar("127.0.0.1", 2995);
+			// TODO: cambiar la forma de autenticarse
+			s = p.identificar("admin", "admin123");
+			// Se obtiene el beneficiario a partir del NSS introducido
+			beneficiario = p.getBeneficiarioPorNSS(s.getId(), Nss);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UsuarioIncorrectoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return SUCCESS;
 	}
 	
@@ -21,6 +54,10 @@ public class loginBeneficiario extends ActionSupport{
 
 	public String getNss() {
 		return Nss;
+	}
+
+	public Beneficiario getBeneficiario() {
+		return beneficiario;
 	}
 
 }
